550cb1f85ac0f44aeda6ef150b234d6a0e9a1808
CitizenGoose <52697132+CitizenGoose@users.noreply.github.com>
2020-11-27 18:19:13 +0100

    Update Ej1.cpp
    Hecho

--- Practica 4/Ej1.cpp
+++ Practica 4/Ej1.cpp
@@ -1,4 +1,25 @@
-/*
-Ahora en un momento lo subo
--Tomas
-*/
+#include <iostream>
+
+using namespace std;
+
+void sumaDigitos(unsigned long x){
+    int sum = 0;
+    while(x != 0){
+        sum += x%10;
+        x /= 10;
+    }
+    
+    cout << sum;
+    }    
+
+int main () {
+
+    unsigned long num;
+
+    cout << "El numero: ";
+    cin >> num;
+
+    sumaDigitos(num);
+
+    return 0;
+}
